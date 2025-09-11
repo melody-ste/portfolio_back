bc669d4dd373dc136712235eef3fcbc7a77bdd07
melody-ste <melody_stephan@icloud.com>
2025-09-11 18:23:07 +0200

    adding header field

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -425,6 +425,7 @@ export interface ApiProjectProject extends Struct.CollectionTypeSchema {
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
     description: Schema.Attribute.Text;
+    header: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
